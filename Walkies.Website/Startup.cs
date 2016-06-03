09e376e687c13c9cdf1a12a2277dade1936d0e2e
Craig <craig@augisoft.com.au>
2016-06-04 09:41:12 +1000

    Add project files.

---
+++ Walkies.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Walkies.Website.Startup))]
+namespace Walkies.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
